ce7f1b8ce48d6f929372c8aa3e789051b6d2c7fb
Bridget Kelly <bkelly@ucdavis.edu>
2021-02-17 14:27:07 -0800

    Implements navigation and adds new icons

--- CashCow/CashCow/LoginViewController.swift
+++ CashCow/CashCow/LoginViewController.swift
@@ -16,13 +16,28 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
         
-        self.set(button: googleLoginButton, image: UIImage(named: "ic_google") ?? UIImage(), with: " Sign In with Google")
-        self.set(button: facebookLoginButton, image: UIImage(named: "ic_facebook") ?? UIImage(), with: " Sign In with Facebook")
+        self.set(button: googleLoginButton, image: UIImage(named: "ic_google") ?? UIImage(), with: "  Sign In with Google")
+        self.set(button: facebookLoginButton, image: UIImage(named: "ic_facebook") ?? UIImage(), with: "  Sign In with Facebook")
         facebookLoginButton.backgroundColor = UIColor(red: 59/255.0, green: 89/255.0, blue: 152/255.0, alpha: 1)
-        self.set(button: emailLoginButton, image: UIImage(named: "ic_email") ?? UIImage(), with: " Sign In with Email")
+        self.set(button: emailLoginButton, image: UIImage(named: "ic_email") ?? UIImage(), with: "  Sign In with Email")
     }
-
+    
+    @IBAction func loginEmailPress() {
+        print("hello!")
+        // Nav to home view for now until login w/ email is implemented
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let homeViewController =  storyboard.instantiateViewController(identifier: "homeViewController") as? HomeViewController else {
+            assertionFailure("Couldn't find VC")
+            return
+        }
+        
+        // Only have home view on stack so user can't go back
+        let viewControllers = [homeViewController]
+        self.navigationController?.setViewControllers(viewControllers, animated: true)
+    }
+    
     func set(button: AZSocialButton, image: UIImage, with text: String) {
       let attachment = NSTextAttachment()
       attachment.image = image
