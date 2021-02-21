8911a7869a851177f72348d66c0f75e3138f4889
Angela Ni <32720944+angieenee@users.noreply.github.com>
2021-02-21 15:06:58 -0800

    added constraints to clicker vc

--- CashCow/CashCow/ClickerViewController.swift
+++ CashCow/CashCow/ClickerViewController.swift
@@ -13,16 +13,27 @@ class ClickerViewController: UIViewController {
     @IBOutlet weak var settingsButton: UIButton!
     @IBOutlet weak var profileButton: UIButton!
     @IBOutlet weak var upgradesButton: UIButton!
+    @IBOutlet weak var clickerButton: UIButton!
+    
+    // M1: Stamina defaults to 5 when view loads
+    int stamina = 5
+    // M1: save user's income
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // TODO: display user's saved income
+        // TODO: display user's saved income, stamina
+        
+        
     }
     
     // TODO: clicker functionality
+    @IBAction func cowClicked(_ sender: Any) {
+    }
     
     // TODO: update stamina bar
-    
-    // TODO: navigate to profile, upgrades, settings
+    func updateStamina {
+        
+    }
+
 }
