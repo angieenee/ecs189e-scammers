2760ac8ce8439c7210d201985d651d2b7e3a628a
Rachel Quan <rachelquan2094@gmail.com>
2021-02-23 16:19:09 -0800

    feat: logic on configuring amounts & setting amounts to text labels

--- CashCow/CashCow/UpgradesViewController.swift
+++ CashCow/CashCow/UpgradesViewController.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class UpgradesViewController: UIViewController {
-
+    
+    // Icon buttons
+    
+    // TODO: use FontAwesome
+    @IBOutlet weak var coffeeIcon: UIButton!
+    @IBOutlet weak var diningIcon: UIButton!
+    @IBOutlet weak var sleepIcon: UIButton!
+    
+    @IBOutlet weak var briefcaseIcon: UIButton!
+    @IBOutlet weak var computerIcon: UIButton!
+    
+    // Amount labels
+    @IBOutlet weak var coffeeAmountLabel: UILabel!
+    
+    // Progress bars
+    
+    
+    // Initialize consts for amounts here
+    let coffeeAmount = "5a"
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // TODO: check if this actually works lol
+        var amountsList = [self.coffeeAmount]
+        
+        let amountLabelsList = [self.coffeeAmountLabel]
+                
+        // Prepend $ to all consts using map
+        amountsList = amountsList.map{"$" + $0}
+                
+        // Set amount labels to respective consts
+        for (amountLabel, amounts) in zip(amountLabelsList, amountsList) {
+            amountLabel?.text = amounts
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
