20b03bdf481f28037005a65055b42c463c3a8b1b
Rachel Quan <rachelquan2094@gmail.com>
2021-02-23 16:18:28 -0800

    feat: clicker -> upgrades, profile views

--- CashCow/CashCow/ClickerViewController.swift
+++ CashCow/CashCow/ClickerViewController.swift
@@ -27,6 +27,30 @@ class ClickerViewController: UIViewController {
         
     }
     
+    @IBAction func profileButtonPressed(_ sender: Any) {
+        // Go to profile view
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let profileViewController =  storyboard.instantiateViewController(identifier: "profileViewController") as? ProfileViewController else {
+            assertionFailure("Couldn't find Profile VC")
+            return
+        }
+        
+        // Push to stack because we want users to be able to go back to clicker view
+        self.navigationController?.pushViewController(profileViewController, animated: true)
+    }
+    
+    @IBAction func upgradesButtonPressed(_ sender: Any) {
+        // Go to upgrades view
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let upgradesViewController =  storyboard.instantiateViewController(identifier: "upgradesViewController") as? UpgradesViewController else {
+            assertionFailure("Couldn't find Upgrades VC")
+            return
+        }
+        
+        // Push to stack because we want users to be able to go back to clicker view
+        self.navigationController?.pushViewController(upgradesViewController, animated: true)
+    }
+    
     @IBAction func settingsButtonPressed(_ sender: Any) {
         // Go to settings view
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -38,7 +62,6 @@ class ClickerViewController: UIViewController {
         // Push to stack because we want users to be able to go back to clicker view
         self.navigationController?.pushViewController(settingsViewController, animated: true)
     }
-    
     
     // TODO: clicker functionality
     @IBAction func cowClicked(_ sender: Any) {
