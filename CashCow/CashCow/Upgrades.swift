714d1dda66dd8baefa70a7bd9e33c49b284bde75
Angela Ni <32720944+angieenee@users.noreply.github.com>
2021-03-03 12:30:16 -0800

    added JSON file with upgrade info

--- CashCow/CashCow/Upgrades.swift
+++ CashCow/CashCow/Upgrades.swift
@@ -10,7 +10,10 @@ import Foundation
 class Upgrade {
     var type: String? // 'stamina', 'passive', or 'perclick"
     var name: String?
-    var amount: Int?
+    var cost: Int?
+    var costCurrency: String?
+    var statAmt: Int?
+    var statAmtCurrency: String?
     var description: String?
     var iconName: String?
 }
