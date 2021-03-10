3c571958068699119ee54195f9d4a2cf61d0e044
Angela Ni <32720944+angieenee@users.noreply.github.com>
2021-03-10 11:18:02 -0800

    added third option to popup

--- CashCow/CashCow/Controllers/PopUpViewController.swift
+++ CashCow/CashCow/Controllers/PopUpViewController.swift
@@ -16,12 +16,18 @@ class PopUpViewController: UIViewController {
     }
 
     @IBAction func option1Pressed() {
-        self.user?.money?.moneyClick = self.user?.money?.add(self.user?.money?.moneyClick ?? ["_": 0], ["_": 0, "A": 5]) ?? ["_": 0]
+        // self.user?.money?.moneyClick = self.user?.money?.add(self.user?.money?.moneyClick ?? ["_": 0], ["_": 0, "A": 5]) ?? ["_": 0]
+        // TODO: insert -25% stamina option
         self.dismiss(animated: true)
     }
     
     @IBAction func option2Pressed() {
-        self.user?.money?.moneyPassive = self.user?.money?.add(self.user?.money?.moneyPassive ?? ["_": 0], ["_": 0, "A": 5])
+        // self.user?.money?.moneyPassive = self.user?.money?.add(self.user?.money?.moneyPassive ?? ["_": 0], ["_": 0, "A": 5])
+        self.user?.money?.balance = self.user?.money?.subtract(self.user?.money?.balance ?? ["_": 0], ["_": 0, "A": 1]) ?? ["_": 0]
         self.dismiss(animated: true)
+    }
+    
+    @IBAction func option3Pressed(_ sender: Any) {
+        self.user?.money?.moneyClick = self.user?.money?.subtract(self.user?.money?.moneyClick ?? ["_": 0], ["_": 100]) ?? ["_": 0]
     }
 }
