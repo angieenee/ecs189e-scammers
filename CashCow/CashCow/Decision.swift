68234ff996a359b25478b2e9699accdc43d24ba8
Angela Ni <32720944+angieenee@users.noreply.github.com>
2021-03-14 14:45:23 -0700

    added sub/add for Decisions

--- CashCow/CashCow/Decision.swift
+++ CashCow/CashCow/Decision.swift
@@ -7,18 +7,16 @@
 
 import Foundation
 
-// Choose A or B decision
 class Decision {
     var id: Int?
     var name: String?
     var description: String?
     var numOptions: Int?
     
-    // ex: Balance ["balance": ["_": 100, "A": 1]
-    // ex: Stamina (use "_" for progress) ["stamina": ["_": 100]
-    var option1change: [String: [String: Int]]?
-    var option2change: [String: [String: Int]]?
-    var option3change: [String: [String: Int]]?
+    // ex: Balance ["balance": ["subtract: ["_": 100, "A": 1]]
+    var option1change: [String: [String: [String: Int]]]?
+    var option2change: [String: [String: [String: Int]]]?
+    var option3change: [String: [String: [String: Int]]]?
     
     var option1text: String?
     var option2text: String?
@@ -29,9 +27,9 @@ class Decision {
         self.name = data["name"] as? String
         self.description = data["description"] as? String
         self.numOptions = data["numOptions"] as? Int
-        self.option1change = data["option1change"] as? [String: [String: Int]]
-        self.option2change = data["option2change"] as? [String: [String: Int]]
-        self.option3change = data["option3change"] as? [String: [String: Int]]
+        self.option1change = data["option1change"] as? [String: [String: [String: Int]]]
+        self.option2change = data["option2change"] as? [String: [String: [String: Int]]]
+        self.option3change = data["option3change"] as? [String: [String: [String: Int]]]
         self.option1text = data["option1text"] as? String
         self.option2text = data["option2text"] as? String
         self.option3text = data["option3text"] as? String
